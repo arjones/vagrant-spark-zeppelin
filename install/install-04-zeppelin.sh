fb118063cd66da322ce4160edb67c85775ae31b4
Fanilo Andrianasolo <fanilo.andrianasolo@worldline.com>
2017-02-08 12:51:49 +0100

    Zeppelin 0.6.2 is in archives now -_-

--- install/install-04-zeppelin.sh
+++ install/install-04-zeppelin.sh
@@ -19,6 +19,7 @@ wget -c "http://archive.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/
 tar zxvf apache-maven-$MAVEN_VERSION-bin.tar.gz -C /usr/
 ln -s ${MAVEN_HOME} /usr/maven
 
+wget -c "http://archive.apache.org/dist/zeppelin/zeppelin-${ZEPPELIN_VERSION}/zeppelin-${ZEPPELIN_PACKAGE}.tgz"
 wget -c "http://apache.mirrors.ovh.net/ftp.apache.org/dist/zeppelin/zeppelin-${ZEPPELIN_VERSION}/zeppelin-${ZEPPELIN_PACKAGE}.tgz"
 tar zxvf zeppelin-${ZEPPELIN_PACKAGE}.tgz -C /usr/
 ln -s ${ZEPPELIN_HOME} /usr/zeppelin
